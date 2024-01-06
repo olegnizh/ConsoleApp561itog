8de3a538ea757f984d7075a7538ee1681a1aa5de
olegnizh <olegnizh@mail.ru>
2024-01-06 12:44:11 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp561itog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
